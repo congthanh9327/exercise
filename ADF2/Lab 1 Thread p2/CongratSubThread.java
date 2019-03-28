94ec3a789e4cf1e6ebf2cdf1609b2ccde3e5e32e
Công Thành <thanh.hc@3si.vn>
2019-03-28 13:58:13 +0700

    Lab 1 Thread p2

---
+++ ADF2/Lab 1 Thread p2/CongratSubThread.java
@@ -0,0 +1,14 @@
+public class CongratSubThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Merry Christmas!");
+    }
+}
