1c4bc172ba8fd05729b2181ee2958dc761d65dfc
Công Thành <thanh.hc@3si.vn>
2019-03-28 12:50:31 +0700

    Assignment 4

---
+++ ADF2/Assignment 4/BanGaoThread.java
@@ -0,0 +1,11 @@
+public class BanGaoThread extends Thread {
+    public KhoGao khoGao = new KhoGao();
+
+    public BanGaoThread(KhoGao kg){
+        this.khoGao = kg;
+    }
+
+    public void run(){
+        this.khoGao.banGao(30);
+    }
+}
---
+++ ADF2/Assignment 4/NhapGaoThread.java
@@ -0,0 +1,11 @@
+public class NhapGaoThread extends Thread {
+    public KhoGao khoGao = new KhoGao();
+
+    public NhapGaoThread(KhoGao kg){
+        this.khoGao = kg;
+    }
+
+    public void run(){
+        this.khoGao.nhapGao(50);
+    }
+}
