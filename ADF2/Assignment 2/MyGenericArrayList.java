f2a13616cb079cfcf45977789463174f6feae514
Công Thành <congthanhhus@gmail.com>
2019-03-28 08:14:08 +0700

    Assignment 2

---
+++ ADF2/Assignment 2/MyGenericArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class MyGenericArrayList {
+    public ArrayList<Integer> arr;
+
+    public ArrayList<Integer> getArr() {
+        return arr;
+    }
+
+    public void setArr(ArrayList<Integer> arr) {
+        this.arr = arr;
+    }
+}
