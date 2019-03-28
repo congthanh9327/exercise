5469ddf69d7b5d10ac7f741ac4cac151bc19a55e
Công Thành <thanh.hc@3si.vn>
2019-03-28 13:58:46 +0700

    Assignment 3 Thread

---
+++ ADF2/Assignment 3 Thread/SubThread2.java
@@ -0,0 +1,11 @@
+public class SubThread2 implements Runnable {
+    @Override
+    public void run() {
+        for (int i=1;i<51;i++){
+            System.out.print(i+" ");
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){}
+        }
+    }
+}
