1bfd4c7226e0af74f4adabf7c47bdfd03a22b60d
Công Thành <congthanhhus@gmail.com>
2019-05-25 07:51:23 +0700

    add assignment 4

---
+++ CSharp/Assignment4/News.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class News : INews
+    {
+        private int id;
+        private string title;
+        private string publishDate;
+        private string author;
+        private string content;
+        private float averageRate;
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PublishDate { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public float AverageRate { get; }
+
+        public void Display()
+        {
+            Console.WriteLine("Id: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("PublishDate: " + this.publishDate);
+            Console.WriteLine("Author: " + this.author);
+            Console.WriteLine("Content: " + this.content);
+            Console.WriteLine("AverageRate: " + this.averageRate);
+            Console.WriteLine();
+        }
+
+        int [] RateList = new int [3];
+
+        public void Calculate()
+        {
+            int total = 0;
+            for (int i = 0; i < RateList.Length; i++)
+            {
+                total += RateList[i];
+            }
+            this.averageRate = total / 3;
+        }
+
+        public News()
+        {
+        }
+
+        public News(string title, string publishDate, string author, string content, int[] rateList)
+        {
+            this.title = title;
+            this.publishDate = publishDate;
+            this.author = author;
+            this.content = content;
+            RateList = rateList;
+        }
+    }
+}
+
---
+++ CSharp/Assignment4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<News> listNews = new List<News>();
+            int flag = 0;
+            do
+            {
+                Console.WriteLine("1. Insert news");
+                Console.WriteLine("2. View list news");
+                Console.WriteLine("3. Average rate");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Please choose an option");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Title: ");
+                        string title = Console.ReadLine();
+                        Console.WriteLine("Publish date: ");
+                        string publishDate = Console.ReadLine();
+                        Console.WriteLine("Author: ");
+                        string author = Console.ReadLine();
+                        Console.WriteLine("Content: ");
+                        string content = Console.ReadLine();
+                        Console.WriteLine("Rate List: ");
+                        int[] rateList = new int[3];
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine("value no "+(i+1));
+                            rateList[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        News news = new News(title, publishDate, author, content, rateList);
+                        listNews.Add(news);
+                        break;
+                    case 2:
+                        foreach (News newsItem in listNews)
+                        {
+                            newsItem.Display();
+                        }
+                        break;
+                    case 3:
+                        foreach (var newsItem in listNews)
+                        {
+                            newsItem.Calculate();
+                            newsItem.Display();
+                        }
+                        break;
+                    case 4:
+                        flag = 1;
+                        break;
+                }
+            }
+            while (flag == 0);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
