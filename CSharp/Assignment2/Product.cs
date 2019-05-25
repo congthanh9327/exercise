78d4f9fa2f18fce2f206e07e5d280e2445260c14
Công Thành <congthanhhus@gmail.com>
2019-05-25 07:50:51 +0700

    add assignment 2

---
+++ CSharp/Assignment2/Product.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Product
+    {
+        public int id;
+        public string name;
+        public int price;
+        public int qty;
+        public string image;
+        public string desc;
+        List <string> gallery;
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name, int price, int qty, string image, string desc, List<string> gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            this.gallery = gallery;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine(this.id);
+            Console.WriteLine(this.name);
+            Console.WriteLine(this.price);
+            Console.WriteLine(this.qty);
+            Console.WriteLine(this.image);
+            Console.WriteLine(this.desc);
+            foreach(string stringGallery in gallery)
+            {
+                Console.WriteLine(stringGallery);
+            }
+        }
+
+        public void inventoryNotice(int qty)
+        {
+            if(qty==0)
+            {
+                Console.WriteLine("Out of stock");
+            }
+            else
+            {
+                Console.WriteLine("In stock");
+            }
+        }
+
+        public void insertImageIntoGallery()
+        {
+            if (gallery.Count() >= 10)
+            {
+                Console.WriteLine("Please delete before insert");
+            }
+            else
+            {
+                Console.WriteLine("Image Inserted: ");
+                string imageInserted = Console.ReadLine();
+                gallery.Add(imageInserted);
+            }
+        }
+
+        public void deleteImageFromGallery()
+        {
+            foreach(string stringGallery in gallery)
+            {
+                Console.WriteLine(stringGallery);
+            }
+            Console.WriteLine("Choose an image to delete: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            gallery.RemoveAt(choice);
+            Console.WriteLine("Delete successful");
+        }
+    }
+}
---
+++ CSharp/Assignment2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            string name = "Book";
+            int price = 1000;
+            int qty= 50;
+            string image= "Mona Lisa";
+            string desc = "This book is awesome";
+            string[] book = { "doraemon", "Teppi", "DragonBall", "OnePunchMan", "doraemon", "Teppi", "DragonBall", "OnePunchMan", "doraemon", "Teppi", "DragonBall", "OnePunchMan" };
+            List<string> gallery = new List<string>(book);
+
+            int id2 = 2;
+            string name2 = "Toy";
+            int price2 = 1000;
+            int qty2 = 50;
+            string image2 = "Mona Lisa";
+            string desc2 = "This book is awesome";
+            string[] book2 = { "doraemon", "Teppi", "DragonBall", "OnePunchMan", "doraemon", "Teppi", "DragonBall", "OnePunchMan", "doraemon", "Teppi", "DragonBall", "OnePunchMan" };
+            List<string> gallery2 = new List<string>(book);
+
+            int id3 = 3;
+            string name3 = "Desk";
+            int price3 = 1000;
+            int qty3 = 50;
+            string image3 = "Mona Lisa";
+            string desc3 = "This book is awesome";
+            string[] book3 = { "doraemon", "Teppi", "DragonBall", "OnePunchMan", "doraemon", "Teppi", "DragonBall", "OnePunchMan", "doraemon", "Teppi", "DragonBall", "OnePunchMan" };
+            List<string> gallery3 = new List<string>(book);
+
+            Product product = new Product(id, name, price, qty, image, desc, gallery);
+            Product product2 = new Product(id2, name2, price2, qty2, image2, desc2, gallery2);
+            Product product3 = new Product(id3, name3, price3, qty3, image3, desc3, gallery3);
+
+            int idCart = 1;
+            string customerCart = "TruongDX8";
+            int grandTotalCart = 5000;
+            Product[] products = { product, product2 };
+            List<Product> productListCart = new List<Product>(products);
+            string cityCart = "Hanoi";
+            string countryCart = "Vietnam";
+            Cart cart = new Cart(idCart, customerCart, grandTotalCart, productListCart, cityCart, countryCart);
+            int flag = 0;
+            do
+            {
+                Console.WriteLine("1. Insert product into cart");
+                Console.WriteLine("2. Delete prodct from cart");
+                Console.WriteLine("3. Total amount");
+                Console.WriteLine("4. Show products");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Please choose an option");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        cart.insertProductIntoCart(product3);
+                        break;
+                    case 2:
+                        cart.deleteProductFromCart();
+                        break;
+                    case 3:
+                        cart.calculateGrandTotal();
+                        break;
+                    case 4:
+                        cart.showProductFromCart();
+                        break;
+                    case 5:
+                        flag = 1;
+                        break;
+                }
+            }
+            while (flag == 0);
+
+            Console.ReadKey();
+        }
+    }
+}
