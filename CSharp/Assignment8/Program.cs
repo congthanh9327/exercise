bba7dacdc93b1e81e9b126d16a80f7ae38d5b248
Công Thành <congthanhhus@gmail.com>
2019-05-25 07:52:18 +0700

    add assignment 8

---
+++ CSharp/Assignment8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            MyGenericArrayList<string, int> myGenericArrayList = new MyGenericArrayList<string, int>();
+            myGenericArrayList.key = "abc";
+            myGenericArrayList.value = 10;
+            MyGenericArrayList<string, int> myGenericArrayList2 = new MyGenericArrayList<string, int>();
+            myGenericArrayList2.key = "cbc";
+            myGenericArrayList2.value = 5;
+
+            arrayList.Add(myGenericArrayList);
+            arrayList.Add(myGenericArrayList2);
+
+            foreach(MyGenericArrayList<string,int> myGeneric in arrayList)
+            {
+                Console.WriteLine(myGeneric.key);
+                Console.WriteLine(myGeneric.value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyGenericArrayList<K,V>
+    {
+        public K key;
+        public V value;
+    }
+}
