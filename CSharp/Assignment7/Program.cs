7bc33783867283184c7fe28f179a50c9c00b37b0
Công Thành <congthanhhus@gmail.com>
2019-05-25 07:52:02 +0700

    add assignment 7

---
+++ CSharp/Assignment7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    
+    class Program
+    {
+        
+        public static void showStringProgram(string x)
+        {
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            Programs program = new Programs();
+            program.Click += showStringProgram;
+            program.Price = 20;
+            program.Price = 22;
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ CSharp/Assignment7/Programs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    class Programs
+    {
+        public delegate void showString(string s);
+        public event showString Click;
+        private String name;
+        private int price;
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (this.price!=0 && this.price != value)
+                {
+                    if (Click != null)
+                    {
+                        Click("Price was changed");
+                    }
+                }
+                this.price = value;
+            }
+        }
+    }
+}
